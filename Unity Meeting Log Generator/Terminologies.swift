c93b1608eb0bca877a9dde17eb1aeb12c55ca0c4
i_jemin <jemin.lee@unity3d.com>
2023-09-18 17:33:07 +0900

    Add predefined terminologies

---
+++ Unity Meeting Log Generator/Terminologies.swift
@@ -0,0 +1,13 @@
+//
+//  Terminologies.swift
+//  Unity Meeting Log Generator
+//
+//  Created by Unity Jemin on 2023/09/18.
+//
+
+import Foundation
+
+struct Terminologies{
+    static let Words = ["유니티", "IL2CPP", "컴파일러"]
+
+}
