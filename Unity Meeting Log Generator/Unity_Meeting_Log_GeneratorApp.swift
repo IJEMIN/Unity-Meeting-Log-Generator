e2723ef1bcf7ca1c15db4c4c8a63f3755de121c3
i_jemin <jemin.lee@unity3d.com>
2023-09-18 15:01:22 +0900

    Initial Commit

---
+++ Unity Meeting Log Generator/Unity_Meeting_Log_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unity_Meeting_Log_GeneratorApp.swift
+//  Unity Meeting Log Generator
+//
+//  Created by Unity Jemin on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct Unity_Meeting_Log_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
